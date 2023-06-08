d1945f2b8ebba09c06fe1a495d2ef5b8cb1750d4
WhiteMorro1177 <jackforest228@gmail.com>
2023-06-08 18:09:34 +0300

    stopwatch added

--- app/src/main/java/ru/mirea/tsybulko/mieraproject/ui/stopwatch/StopwatchFragment.kt
+++ app/src/main/java/ru/mirea/tsybulko/mieraproject/ui/stopwatch/StopwatchFragment.kt
@@ -1,19 +1,26 @@
 package ru.mirea.tsybulko.mieraproject.ui.stopwatch
 
 import android.annotation.SuppressLint
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import ru.mirea.tsybulko.mieraproject.R
 import ru.mirea.tsybulko.mieraproject.databinding.FragmentStopwatchBinding
+import kotlin.math.roundToInt
 
 class StopwatchFragment : Fragment() {
-    private lateinit var binder: FragmentStopwatchBinding
+    private lateinit var binding: FragmentStopwatchBinding
     private var isStopwatchActive = false
+    private lateinit var serviceIntent: Intent
+    private var time = 0.0
+
+    private lateinit var fragment: Context
 
     @SuppressLint("SetTextI18n")
     override fun onCreateView(
@@ -21,47 +28,59 @@ class StopwatchFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val rootFragmentView = inflater.inflate(R.layout.fragment_stopwatch, container, false)
-        binder = FragmentStopwatchBinding.inflate(layoutInflater)
+        binding = FragmentStopwatchBinding.bind(rootFragmentView)
 
-        binder.startButton.setOnClickListener {
-            if (!isStopwatchActive) {
-                startStopwatch()
-                isStopwatchActive = true
-            }
-        }
+        fragment = this.context!!
 
-        binder.stopButton.setOnClickListener {
-            if (isStopwatchActive) {
-                pauseStopwatch()
-                isStopwatchActive = false
-            }
-        }
+        binding.startStopButton.setOnClickListener { startStopTimer() }
+        binding.resetButton.setOnClickListener { resetTimer() }
 
-        binder.resetButton.setOnClickListener {
-            if (!isStopwatchActive) { resetStopwatch() }
-        }
-
+        serviceIntent = Intent(this.context, StopwatchService::class.java)
+        fragment.registerReceiver(updateTime, IntentFilter(StopwatchService.TIMER_UPDATED))
         return rootFragmentView
     }
 
-    private fun startStopwatch() {
-        Log.d("sw", "started")
-        val stopwatchService = Intent(context, StopwatchService::class.java)
-        stopwatchService.putExtra(StopwatchService.STOPWATCH_ACTION, StopwatchService.START)
-        context?.startService(stopwatchService)
+    private fun resetTimer() {
+        stopTimer()
+        time = 0.0
+        binding.timeTV.text = getTimeStringFromDouble(time)
     }
 
-    private fun pauseStopwatch() {
-        Log.d("sw", "paused")
-        val stopwatchService = Intent(context, StopwatchService::class.java)
-        stopwatchService.putExtra(StopwatchService.STOPWATCH_ACTION, StopwatchService.PAUSE)
-        context?.startService(stopwatchService)
+    private fun startStopTimer() = if (isStopwatchActive) stopTimer() else startTimer()
+
+    @SuppressLint("UseCompatLoadingForDrawables", "SetTextI18n")
+    private fun startTimer() {
+        serviceIntent.putExtra(StopwatchService.TIME_EXTRA, time)
+        fragment.startService(serviceIntent)
+        binding.startStopButton.text = "Stop"
+        binding.startStopButton.icon = fragment.getDrawable(R.drawable.baseline_pause_24)
+        isStopwatchActive = true
     }
 
-    private fun resetStopwatch() {
-        Log.d("sw", "reset")
-        val stopwatchService = Intent(context, StopwatchService::class.java)
-        stopwatchService.putExtra(StopwatchService.STOPWATCH_ACTION, StopwatchService.RESET)
-        context?.startService(stopwatchService)
+    @SuppressLint("UseCompatLoadingForDrawables", "SetTextI18n")
+    private fun stopTimer() {
+        fragment.stopService(serviceIntent)
+        binding.startStopButton.text = "Start"
+        binding.startStopButton.icon = fragment.getDrawable(R.drawable.baseline_play_arrow_24)
+        isStopwatchActive = false
     }
+
+    private val updateTime: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            time = intent.getDoubleExtra(StopwatchService.TIME_EXTRA, 0.0)
+            binding.timeTV.text = getTimeStringFromDouble(time)
+        }
+    }
+
+    private fun getTimeStringFromDouble(time: Double): String {
+        val resultInt = time.roundToInt()
+        val hours = resultInt % 86400 / 3600
+        val minutes = resultInt % 86400 % 3600 / 60
+        val seconds = resultInt % 86400 % 3600 % 60
+
+        return makeTimeString(hours, minutes, seconds)
+    }
+
+    private fun makeTimeString(hour: Int, min: Int, sec: Int): String =
+        String.format("%02d:%02d:%02d", hour, min, sec)
 }
--- app/src/main/java/ru/mirea/tsybulko/mieraproject/ui/stopwatch/StopwatchService.kt
+++ app/src/main/java/ru/mirea/tsybulko/mieraproject/ui/stopwatch/StopwatchService.kt
@@ -1,181 +1,46 @@
 package ru.mirea.tsybulko.mieraproject.ui.stopwatch
 
-import android.annotation.SuppressLint
 import android.app.*
 import android.content.Intent
 import android.os.IBinder
 import android.util.Log
-import androidx.core.app.NotificationCompat
-import androidx.core.content.ContextCompat
 import java.util.*
+
 
 class StopwatchService : Service() {
 
-    private lateinit var notificationManager: NotificationManager
-    private lateinit var stopwatchTimer: Timer
-    private lateinit var updateTimer: Timer
+    override fun onBind(p0: Intent?): IBinder? = null
 
+    private val timer = Timer()
 
-    private var isStopWatchRunning = false
-    private var timeElapsed = 0
-
-
-    companion object {
-        // Channel ID for notifications
-        const val CHANNEL_ID = "Stopwatch_Notifications"
-
-        // Service Actions
-        const val START = "START"
-        const val PAUSE = "PAUSE"
-        const val RESET = "RESET"
-        const val GET_STATUS = "GET_STATUS"
-
-        // Intent Extras
-        const val STOPWATCH_ACTION = "STOPWATCH_ACTION"
-        const val TIME_ELAPSED = "TIME_ELAPSED"
-        const val IS_STOPWATCH_RUNNING = "IS_STOPWATCH_RUNNING"
-
-        // Intent Actions
-        const val STOPWATCH_TICK = "STOPWATCH_TICK"
-        const val STOPWATCH_STATUS = "STOPWATCH_STATUS"
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int
+    {
+        val time = intent.getDoubleExtra(TIME_EXTRA, 0.0)
+        timer.scheduleAtFixedRate(TimeTask(time), 0, 1000)
+        Log.d("StopwatchFragment", "onStartCommand is running")
+        return START_NOT_STICKY
     }
 
-    override fun onBind(p0: Intent?): IBinder? {
-        Log.d("Stopwatch", "Stopwatch onBind")
-        return null
+    override fun onDestroy()
+    {
+        timer.cancel()
+        super.onDestroy()
     }
 
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        createChannel()
-        getNotificationManager()
-
-        val action = intent?.getStringExtra(STOPWATCH_ACTION)!!
-
-        Log.d("Stopwatch", "onStartCommand Action: $action")
-
-        when (action) {
-            START -> startStopwatch()
-            PAUSE -> pauseStopwatch()
-            RESET -> resetStopwatch()
-            GET_STATUS -> sendStatus()
-        }
-
-        return START_STICKY
-    }
-
-    private fun createChannel() {
-        val notificationChannel = NotificationChannel(
-            CHANNEL_ID,
-            "Stopwatch",
-            NotificationManager.IMPORTANCE_DEFAULT
-        )
-
-        notificationChannel.setShowBadge(true)
-        getSystemService(NotificationManager::class.java).createNotificationChannel(
-            notificationChannel
-        )
-    }
-
-    private fun getNotificationManager() {
-        notificationManager = ContextCompat.getSystemService(
-            this,
-            NotificationManager::class.java
-        ) as NotificationManager
-    }
-
-    private fun sendStatus() {
-        sendBroadcast(Intent().apply {
-            action = STOPWATCH_STATUS
-            putExtra(IS_STOPWATCH_RUNNING, isStopWatchRunning)
-            putExtra(TIME_ELAPSED, timeElapsed)
-        })
-    }
-
-    private fun startStopwatch() {
-        isStopWatchRunning = true
-
-        sendStatus()
-
-        stopwatchTimer = Timer()
-        stopwatchTimer.scheduleAtFixedRate(object : TimerTask() {
-            override fun run() {
-                timeElapsed++
-
-                sendBroadcast(Intent().apply {
-                    action = STOPWATCH_TICK
-                    putExtra(TIME_ELAPSED, timeElapsed)
-                })
-            }
-        }, 0, 1000)
-    }
-
-    private fun pauseStopwatch() {
-        stopwatchTimer.cancel()
-        isStopWatchRunning = false
-        sendStatus()
-    }
-
-    private fun resetStopwatch() {
-        pauseStopwatch()
-        timeElapsed = 0
-        sendStatus()
-    }
-
-    @SuppressLint("UnspecifiedImmutableFlag")
-    private fun buildNotification(): Notification {
-        val title = if (isStopWatchRunning) {
-            "Stopwatch is running!"
-        } else {
-            "Stopwatch is paused!"
-        }
-
-        val hours: Int = timeElapsed.div(60).div(60)
-        val minutes: Int = timeElapsed.div(60)
-        val seconds: Int = timeElapsed.rem(60)
-
-        val intent = Intent(this, StopwatchFragment::class.java)
-        val pIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
-
-        return NotificationCompat.Builder(this, CHANNEL_ID)
-            .setContentTitle(title)
-            .setOngoing(true)
-            .setContentText(
-                "${"%02d".format(hours)}:${"%02d".format(minutes)}:${
-                    "%02d".format(
-                        seconds
-                    )
-                }"
-            )
-            .setOnlyAlertOnce(true)
-            .setContentIntent(pIntent)
-            .setAutoCancel(true)
-            .build()
-    }
-
-    private fun updateNotification() {
-        notificationManager.notify(
-            1,
-            buildNotification()
-        )
-    }
-
-    private fun moveToForeground() {
-
-        if (isStopWatchRunning) {
-            startForeground(1, buildNotification())
-
-            updateTimer = Timer()
-
-            updateTimer.scheduleAtFixedRate(object : TimerTask() {
-                override fun run() {
-                    updateNotification()
-                }
-            }, 0, 1000)
+    private inner class TimeTask(private var time: Double) : TimerTask()
+    {
+        override fun run()
+        {
+            val intent = Intent(TIMER_UPDATED)
+            time++
+            intent.putExtra(TIME_EXTRA, time)
+            sendBroadcast(intent)
         }
     }
 
-    private fun moveToBackground() {
-        updateTimer.cancel()
-        stopForeground(true)
+    companion object
+    {
+        const val TIMER_UPDATED = "timerUpdated"
+        const val TIME_EXTRA = "timeExtra"
     }
 }
